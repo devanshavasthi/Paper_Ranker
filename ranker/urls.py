5d65f778e58a046e4f15ed19d28c270ea67c6984
sankalp-99 <sankalpthakur211@gmail.com>
2021-11-02 11:17:34 +0530

    template

--- ranker/urls.py
+++ ranker/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path("search/<str:key>/",views.search,name ="search"),
     path('', views.index, name='index'),
 ]
--- ranker/views.py
+++ ranker/views.py
@@ -1,7 +1,28 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from bs4 import BeautifulSoup as bs
+import selenium
+import requests
 
 # Create your views here.
+
+
+gd = {}
 def index(request):
-	return HttpResponse("Hello World")
+	return render(request,'ranker/index.html')
+
+
+def search(request,key):
+
+	global gd
+	url = "https://arxiv.org/archive/cs"
+	page = requests.get(url)
+	gd['soup']  = bs(page.text,"html.parser")	
+	soup = gd['soup']
+	a = soup.find_all("ul")
+	z = a[3].find_all("li")
+	print(z[0].find("b"))
+	m = z[0].find_all("a")
+	return HttpResponse(m)
+
 
